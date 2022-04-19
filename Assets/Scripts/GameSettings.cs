af1f2568e53c7c63d3c3fc48b511fdae51991802
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-20 00:07:35 +0900

    Added GameSettings.cs.

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    int _settings;
+
+    const int SettingsNumber = 2;
+
+    public enum EPairNumber
+    {
+        NotSet = 0,
+        E10Pairs = 10,
+        E15Pairs = 15,
+        E20Pairs = 20,
+    }
+
+    public enum EPuzzleCategories
+    {
+        NotSet,
+        Type1,
+        Type2,
+    }
+
+    public struct Settings
+    {
+        public EPairNumber PairsNumber;
+        public EPuzzleCategories PuzzleCategory;
+    };
+
+    Settings _gameSettings;
+
+    public static GameSettings Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(target: this);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(obj: this);
+        }
+    }
+    void Start()
+    {
+        _gameSettings = new Settings();
+        ResetGameSettings();
+    }
+
+    public void SetPairNumber(EPairNumber Number)
+    {
+        if (_gameSettings.PairsNumber == EPairNumber.NotSet)
+        {
+            _settings++;
+        }
+
+        _gameSettings.PairsNumber = Number;
+    }
+
+    public void SetPuzzleCategories(EPuzzleCategories cat)
+    {
+        if (_gameSettings.PuzzleCategory == EPuzzleCategories.NotSet)
+        {
+            _settings++;
+        }
+
+        _gameSettings.PuzzleCategory = cat;
+    }
+
+    public EPairNumber GetPairNumber()
+    {
+        return _gameSettings.PairsNumber;
+    }
+
+    public EPuzzleCategories GetPuzzleCategory()
+    {
+        return _gameSettings.PuzzleCategory;
+    }
+
+    public void ResetGameSettings()
+    {
+        _settings = 0;
+        _gameSettings.PuzzleCategory = EPuzzleCategories.NotSet;
+        _gameSettings.PairsNumber = EPairNumber.NotSet;
+    }
+}
