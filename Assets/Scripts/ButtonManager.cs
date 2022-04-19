f5d9a74b405176ba4a0a6eb5ec1fd2e50bc0629f
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-19 09:28:08 +0900

    Added a comment to the Button Manager-Script.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,12 +1,15 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>ボタンを押した時にシーン遷移するスクリプト</summary>
 public class ButtonManager : MonoBehaviour
 {
+    /// <summary>遷移するシーンを読み込む</summary>
+    /// <param name="scene_name"></param>
     public void LoadScene(string scene_name)
     {
-        SceneManager.LoadScene(scene_name);
+        SceneManager.LoadScene(scene_name); // 指定したシーンに移動
     }
 }
