0b8d38b44a89f781592ef6c1df247130aec38428
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-20 23:45:09 +0900

    Added SetGameButton.cs .

---
+++ Assets/Scripts/SetGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameButton : MonoBehaviour
+{
+    public enum EButtonType
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
