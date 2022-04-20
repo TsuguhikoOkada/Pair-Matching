6283d67c10b361aceceaa7370314a0a0bb663ba1
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-20 23:56:04 +0900

    Coded global variables in SetGameButton.cs .

--- Assets/Scripts/SetGameButton.cs
+++ Assets/Scripts/SetGameButton.cs
@@ -6,8 +6,18 @@ public class SetGameButton : MonoBehaviour
 {
     public enum EButtonType
     {
-
+        NotSet,
+        PairNumberBtn,
+        PuzzleCategoryBtn,
     }
+
+    [SerializeField,Header("ボタンのタイプ")]public EButtonType _buttonType = EButtonType.NotSet;
+
+
+    [HideInInspector] public GameSettings.EPairNumber _pairNumber = GameSettings.EPairNumber.NotSet;
+
+
+    [HideInInspector] public GameSettings.EPuzzleCategories _puzzleCategories = GameSettings.EPuzzleCategories.NotSet;
 
     void Start()
     {
